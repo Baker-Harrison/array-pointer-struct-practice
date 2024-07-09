88eaf8359bc300ff44072b9a8ee6e5f9abd5ea80
BachiBachBach <thehessboi@gmail.com>
2024-07-09 11:43:33 -0500

    finally fixed maybe?

---
+++ practice-array-pointers-structs.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+
+using namespace std;
+
+void practiceArrays();
+
+void practicePointers();
+
+void practiceStructs();
+
+struct Suit
+{
+    string name;
+    int value;
+};
+
+struct Rank
+{
+    string name;
+    int value;
+};
+
+struct Card
+{
+    Suit suit;
+    Rank rank;
+    bool isFaceUp;
+};
+
+
+
+int main()
+{
+    // practiceArrays();
+    practicePointers();
+}
+
+void practiceArrays()
+{
+    int SIZE = 0;
+    int *numbers = nullptr;
+
+    numbers = new int[SIZE];
+
+    cout << "How many numbers do you want: ";
+    cin >> SIZE;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << "Give me a number: ";
+        cin >> *(numbers + i);
+    }
+
+    /*
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << *(numbers + i) << endl;
+    }
+    */
+
+    int total = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        total += *(numbers + i);
+    }
+
+    cout << "Total: " << total << endl;
+
+    cout << "Average: " << (total / static_cast<double>(SIZE)) << endl;
+
+    long int max = -3434344324346643;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (*(numbers + i) > max)
+        {
+            max = *(numbers + i);
+        }
+    }
+    cout << "Max: " << max << endl;
+
+    long int min = 33842983592935839;
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (*(numbers + i) < min)
+        {
+            min = *(numbers + i);
+        }
+    }
+    cout << "Min: " << min << endl;
+}
+
+void practicePointers()
+{
+    string *name = new string;
+    int *age = new int;
+
+    cout << "What is your name? ";
+    cin >> *name;
+
+    cout << "What is your age? ";
+    cin >> *age;
+
+    cout << "Name: " << *name << endl;
+    cout << "Name address: " << name << endl;
+
+    cout << "Age: " << *age << endl;
+    cout << "Age address: " << age << endl
+         << endl;
+
+    string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    // Foward
+
+    for (int i = 0; i < 12; i++)
+    {
+        cout << *(months + i) << endl;
+    }
+
+    cout << endl;
+
+    // Backward
+    for (int i = 11; i > -1 ; i--)
+    {
+        cout << *(months + i) << endl;
+    }
+}
+
+void practiceStructs()
+{
+    
+}
